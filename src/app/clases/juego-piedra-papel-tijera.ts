c3bf195f69f943d5ba454c3c0ce7db695f393db5
lucasgabrielsanchez <sanchez_lukas@hotmail.com>
2020-10-08 06:11:05 -0300

    Piedra Papel o Tijera

--- src/app/clases/juego-piedra-papel-tijera.ts
+++ src/app/clases/juego-piedra-papel-tijera.ts
@@ -1,2 +1,63 @@
-export class JuegoPiedraPapelTijera {
+import { Juego } from "../clases/juego";
+
+export class JuegoPiedraPapelTijera extends Juego {
+  arrayOpciones: Array<string>;
+  eleccionOponente: string;
+  eleccionUsuario: string;
+  empato: boolean;
+  perdio: boolean;
+
+  constructor(nombre?: string, gano?: boolean, jugador?: string) {
+    super("Piedra Papel o Tijera", gano, jugador);
+
+    this.arrayOpciones = ["piedra", "papel", "tijera"];
+  }
+
+  public verificar() {
+    this.gano = false;
+    this.perdio = false;
+    this.empato = false;
+
+    if (this.eleccionUsuario == this.eleccionOponente) {
+      this.empato = true;
+      console.info("EMPATO!!!");
+      return false;
+    }
+
+    switch (this.eleccionUsuario) {
+      case "piedra":
+        if (this.eleccionOponente == "tijera") {
+          this.gano = true;
+        }
+        break;
+
+      case "papel":
+        if (this.eleccionOponente == "piedra") {
+          this.gano = true;
+        }
+        break;
+
+      case "tijera":
+        if (this.eleccionOponente == "papel") {
+          this.gano = true;
+        }
+        break;
+    }
+
+    if (this.gano) {
+      console.info("GANO!!!");
+      return true;
+    } else {
+      console.info("PERDIO!!!");
+      this.perdio = true;
+      return false;
+    }
+  }
+
+  public generarEleccionAleatoria() {
+    let aux = Math.floor(Math.random() * 3 + 0);
+    this.eleccionOponente = this.arrayOpciones[aux];
+    console.info("Eleccion oponente:" + this.eleccionOponente);
+    this.gano = false;
+  }
 }
--- src/app/componentes/piedra-papel-tijera/piedra-papel-tijera.component.ts
+++ src/app/componentes/piedra-papel-tijera/piedra-papel-tijera.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import { JuegoPiedraPapelTijera } from "../../clases/juego-piedra-papel-tijera";
 
 @Component({
-  selector: 'app-piedra-papel-tijera',
-  templateUrl: './piedra-papel-tijera.component.html',
-  styleUrls: ['./piedra-papel-tijera.component.css']
+  selector: "app-piedra-papel-tijera",
+  templateUrl: "./piedra-papel-tijera.component.html",
+  styleUrls: ["./piedra-papel-tijera.component.css"],
 })
 export class PiedraPapelTijeraComponent implements OnInit {
+  nuevoJuego: JuegoPiedraPapelTijera;
+  public parametro;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private _route: ActivatedRoute, private _router: Router) {
+    this.nuevoJuego = new JuegoPiedraPapelTijera();
   }
 
+  ngOnInit() {
+    this._route.params.forEach((params: Params) => {
+      //la funcion callback no t permite salir del ambito de la funcion a la clase y no reconoce el parametro 'parametro', por eso se usa la expresion lambda
+      this.parametro = params["valor"];
+      console.log(params);
+    });
+  }
+
+  public generarEleccion() {
+    this.nuevoJuego.generarEleccionAleatoria();
+  }
+
+  public elegirImagen(imagen: string) {
+    this.nuevoJuego.eleccionUsuario = imagen;
+    this.nuevoJuego.verificar();
+  }
+
+  redirigirAMenuJuegos() {
+    this._router.navigate(["/Juegos"]);
+  }
 }
