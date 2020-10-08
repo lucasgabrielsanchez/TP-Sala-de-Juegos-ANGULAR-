30fba69ac136da012186601767442e22eb8da182
lucasgabrielsanchez <sanchez_lukas@hotmail.com>
2020-10-08 07:48:24 -0300

    Anagrama.

--- src/app/componentes/anagrama/anagrama.component.ts
+++ src/app/componentes/anagrama/anagrama.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-anagrama',
-  templateUrl: './anagrama.component.html',
-  styleUrls: ['./anagrama.component.css']
+  selector: "app-anagrama",
+  templateUrl: "./anagrama.component.html",
+  styleUrls: ["./anagrama.component.css"],
 })
 export class AnagramaComponent implements OnInit {
+  palabrasParaAdivinar = [
+    "cartera",
+    "paraguas",
+    "cartel",
+    "mesada",
+    "margarita",
+    "cocodrilo",
+    "puerta",
+    "sonrisa",
+    "celeste",
+    "caracol",
+    "huevo",
+    "cocina",
+    "flamenco",
+  ];
 
-  constructor() { }
+  palabraSecreta: string = "init";
+  palabraSecretaMezclada: string = "";
+  palabraIngresada: string = "";
+  mensajeAlUsuario: boolean = false;
+  gano: boolean = false;
+  temporizador: any;
+  tiempo: any = 3;
+  mostrarAyuda: boolean = false;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngOnDestroy() {}
+
+  nuevoJuego() {
+    this.gano = false;
+    this.palabraSecreta = this.palabrasParaAdivinar[Math.floor(Math.random() * this.palabrasParaAdivinar?.length)];
+
+    console.log('this.palabraSecreta: ', this.palabraSecreta);
+
+    let arrayDeCaracteresOrdenados = [...this.palabraSecreta];
+
+    this.palabraSecretaMezclada = arrayDeCaracteresOrdenados
+      .map((a) => ({ sort: Math.random(), value: a })) //Le agrego una key numerica aleatoria
+      .sort((a, b) => a.sort - b.sort) //Las ordeno de menor a mayor
+      .map((a) => a.value) //Hago una copia de los value al array nuevo
+      .toString()
+      .replace(/[\,]/gm, " ");
   }
 
+  verificar() {
+    if (this.palabraIngresada.toLowerCase() == this.palabraSecreta.toLowerCase()) {
+      this.gano = true;
+      // this.serviceJugadores.gano();
+    } else {
+      // this.serviceJugadores.perdio();
+      this.mensajeAlUsuario = true;
+      this.temporizador = setInterval(() => {
+        //Comienza a correr el tiempo
+        this.tiempo--;
+        console.log("tiempo: ", this.tiempo);
+        if (this.tiempo == 0) {
+          clearInterval(this.temporizador); //Borro el contador
+          this.tiempo = 3;
+          this.mensajeAlUsuario = false;
+        }
+      }, 900);
+    }
+  }
 }
